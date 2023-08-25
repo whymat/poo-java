424c3a74478afe88a854a5011104045abb735b15
Matheus Santos <81287841+whymat@users.noreply.github.com>
2023-08-24 21:17:09 -0300

    Update Caneta.java

--- aula02/Caneta.java
+++ aula02/Caneta.java
@@ -12,6 +12,14 @@ public class Caneta {
         System.out.println("Carga: " + this.carga);
         System.out.println("Está tampada? " + this.tampada);
     }
+
+    void rabiscar(){ // um método para rabiscar com o objeto, dependendo se ele está tampado ou não
+        if (this.tampada == true) {
+            System.out.println("ERRO! Não posso rabiscar");
+        } else {
+            System.out.println("Estou rabiscando");
+        }
+    }
     
     void tampar(){ // um método para que o objeto que chamou (c1 ou c2 / this) seja tampado
         this.tampada = true;
@@ -19,13 +27,5 @@ public class Caneta {
     
     void destampar(){ // um método para que o objeto que chamou (c1 ou c2 / this) seja destampado
         this.tampada = false;
-    }
-    
-    void rabiscar(){ // um método para rabiscar com o objeto, dependendo se ele está tampado ou não
-        if (this.tampada == true) {
-            System.out.println("ERRO! Não posso rabiscar");
-        } else {
-            System.out.println("Estou rabiscando");
-        }
     }
 }
