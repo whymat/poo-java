8720825565abd2074ea2c0f0bbbeeed060946108
Matheus Santos <81287841+whymat@users.noreply.github.com>
2023-08-29 21:53:06 -0300

    aula06

---
+++ aula06/ControleRemoto.java
@@ -0,0 +1,118 @@
+public class ControleRemoto implements Controlador {
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    public ControleRemoto() {
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando = false;
+    }
+
+    private int getVolume() {
+        return volume;
+    }
+
+    private void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    private boolean isLigado() {
+        return ligado;
+    }
+
+    private void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    private boolean isTocando() {
+        return tocando;
+    }
+
+    private void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+    
+    // métodos abstratos
+
+    @Override // sobrescrever, já se tinha um método abstrato definido, mas agora ele será editado
+    public void ligar() {
+        this.setLigado(true);
+    }
+
+    @Override
+    public void desligar() {
+        this.setLigado(false);
+    }
+
+    @Override
+    public void abrirMenu() {
+        if (this.isLigado()) {
+            System.out.println("-----MENU-----");
+            System.out.println("Está ligado? " + this.isLigado());
+            System.out.println("Está tocando? " + this.isTocando());
+            System.out.print("Volume: " + this.getVolume());
+            for (int i = 0; i < 50; i += 10){
+                System.out.print("|");
+            } 
+        } else {
+            System.out.println("Impossível abrir menu!");
+        }
+        System.out.println("");
+    }
+
+    @Override
+    public void fecharMenu() {
+        System.out.println("Fechando menu...");
+    }
+
+    @Override
+    public void maisVolume() {
+        if (this.isLigado()) {
+            this.setVolume(this.getVolume() + 5);
+        } else {
+            System.out.println("Impossível aumentar volume!");
+        }
+    }
+
+    @Override
+    public void menosVolume() {
+        if (this.isLigado()) {
+            this.setVolume(this.getVolume() - 5);
+        } else {
+            System.out.println("Impossível diminuir volume!");
+        }
+    }
+
+    @Override
+    public void ligarMudo() {
+        if (this.isLigado() && this.getVolume() > 0) { // && significa AND
+            this.setVolume(0);
+        }
+    }
+
+    @Override
+    public void desligarMudo() {
+        if (this.isLigado() && this.getVolume() == 0) {
+            this.setVolume(50);
+        }
+    }
+
+    @Override
+    public void play() {
+        if (this.isLigado() && !(this.isTocando())) { // se está ligado e NÃO tocando
+            this.setTocando(true);
+        } else {
+            System.out.println("Não consegui reproduzir!");
+        }
+    }
+
+    @Override
+    public void pause() {
+        if (this.isLigado() && this.isTocando()) {
+            this.setTocando(false);
+        } else {
+            System.out.println("Não consegui pausar!");
+        }
+    }
+}
